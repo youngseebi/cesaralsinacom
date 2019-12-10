b050c23070e31021fde920cc67da9bc07800530c
Sebastian Alsina <alsinas@me.com>
2019-12-10 03:18:21 -0500

    new site!

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    // make the lightbox automatically scroll
+    setInterval(() => {
+        $(".lightbox").scrollLeft($(".lightbox").scrollLeft() + 1);
+    }, 20);
+});
+
+$(function () {
+
+});
